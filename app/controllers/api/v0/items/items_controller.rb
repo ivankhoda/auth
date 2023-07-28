7cd1ad7f3357f6ff301e0bb9601490f9fa0ec441
Ivan Khoda <ivankhoda01@gmail.com>
2023-07-28 19:55:29 +0300

    change namespaces.

--- app/controllers/api/v0/items/items_controller.rb
+++ app/controllers/api/v0/items/items_controller.rb
@@ -1,48 +1,53 @@
 # frozen_string_literal=true
+module Api
+  module V0
+    module Items
+      class ItemsController < ApplicationController
+        before_action :authenticate_user!
+        wrap_parameters :slot, include: [:code, :name, :slot_id]
 
-class Items::ItemsController < ApplicationController
-  before_action :authenticate_user!
-  wrap_parameters :slot, include: [:code, :name, :slot_id]
+        def create
+          @slot = Item.new(item_creation_params)
+          if @slot.save
+            render json: {slot: @slot}
+          else
+            render json: {error: @slot.errors.full_messages}, status: :unprocessable_entity
+          end
+        end
 
-  def create
-    @slot = Item.new(item_creation_params)
-    if @slot.save
-      render json: {slot: @slot}
-    else
-      render json: {error: @slot.errors.full_messages}, status: :unprocessable_entity
+        def index
+          @items = current_user_items
+          render json: {items: @items}
+        end
+
+        def show
+          @slot = current_user_items.find(params[:id])
+          render json: {slot: @slot}
+        end
+
+        def destroy
+          @slot = Item.find(params[:id])
+          if @slot.destroy
+            render status: 204
+          else
+            render json: {errors: @slot.errors}, status: :unprocessable_entity
+          end
+        end
+
+        private
+
+        def current_user_items
+          current_user.items
+        end
+
+        def item_creation_params
+          item_params.merge({user: current_user})
+        end
+
+        def item_params
+          params.require(:slot).permit(:code, :name, :slot_id)
+        end
+      end
     end
-  end
-
-  def index
-    @items = current_user_items
-    render json: {items: @items}
-  end
-
-  def show
-    @slot = current_user_items.find(params[:id])
-    render json: {slot: @slot}
-  end
-
-  def destroy
-    @slot = Item.find(params[:id])
-    if @slot.destroy
-      render status: 204
-    else
-      render json: {errors: @slot.errors}, status: :unprocessable_entity
-    end
-  end
-
-  private
-
-  def current_user_items
-    current_user.items
-  end
-
-  def item_creation_params
-    item_params.merge({user: current_user})
-  end
-
-  def item_params
-    params.require(:slot).permit(:code, :name, :slot_id)
   end
 end
--- app/controllers/api/v0/slots/slots_controller.rb
+++ app/controllers/api/v0/slots/slots_controller.rb
@@ -1,48 +1,53 @@
 # frozen_string_literal = true
+module Api
+  module V0
+    module Slots
+      class SlotsController < ApplicationController
+        before_action :authenticate_user!
+        wrap_parameters :slot, include: [:code, :name, :parent_id]
 
-class Api::V0::Slots::SlotsController < ApplicationController
-  before_action :authenticate_user!
-  wrap_parameters :slot, include: [:code, :name, :parent_id]
+        def create
+          @slot = Slot.new(slot_creation_params)
+          if @slot.save
+            render json: {slot: @slot}
+          else
+            render json: {error: @slot.errors.full_messages}, status: :unprocessable_entity
+          end
+        end
 
-  def create
-    @slot = Slot.new(slot_creation_params)
-    if @slot.save
-      render json: {slot: @slot}
-    else
-      render json: {error: @slot.errors.full_messages}, status: :unprocessable_entity
+        def index
+          @slots = current_user_slots
+          render json: {slots: @slots}
+        end
+
+        def show
+          @slot = current_user_slots.find(params[:id])
+          render json: {slot: @slot}
+        end
+
+        def destroy
+          @slot = Slot.find(params[:id])
+          if @slot.destroy
+            render status: 204
+          else
+            render json: {errors: @slot.errors}, status: :unprocessable_entity
+          end
+        end
+
+        private
+
+        def current_user_slots
+          current_user.slots
+        end
+
+        def slot_creation_params
+          slot_params.merge({user: current_user})
+        end
+
+        def slot_params
+          params.require(:slot).permit(:code, :name, :parent_id)
+        end
+      end
     end
-  end
-
-  def index
-    @slots = current_user_slots
-    render json: {slots: @slots}
-  end
-
-  def show
-    @slot = current_user_slots.find(params[:id])
-    render json: {slot: @slot}
-  end
-
-  def destroy
-    @slot = Slot.find(params[:id])
-    if @slot.destroy
-      render status: 204
-    else
-      render json: {errors: @slot.errors}, status: :unprocessable_entity
-    end
-  end
-
-  private
-
-  def current_user_slots
-    current_user.slots
-  end
-
-  def slot_creation_params
-    slot_params.merge({user: current_user})
-  end
-
-  def slot_params
-    params.require(:slot).permit(:code, :name, :parent_id)
   end
 end
